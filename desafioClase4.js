790620fdf042da20079303c4f28643bd5454d915
Franco Arias <francosamuel1999@gmail.com>
2022-03-22 00:32:10 -0300

    desafio2

--- desafioClase4.js
+++ desafioClase4.js
@@ -5,36 +5,44 @@ const path = './productos.txt'
 class Contenedor {
 
     save = (obj) => {
-        const misProductos = fs.readFileSync(path, 'utf-8')
-        if (misProductos.length === 0) {
-            const iD = misProductos.length + 1
-            const input = {
-                titulo: obj.titulo,
-                precio: obj.precio,
-                id: iD
+        try {
+                const data = fs.readFileSync(path, 'utf-8')
+                const productos = JSON.parse(data)
+                const iD = productos.length + 1
+                const input = {
+                    titulo: obj.titulo,
+                    precio: obj.precio,
+                    id: iD
+                }
+
+                let aux = [
+                    ...productos,
+                    input,
+                ]
+
+                fs.writeFileSync(path, JSON.stringify(aux, null, '\t'))
+            }catch (err) {
+                console.log("error")
             }
-            misProductos.push(input)
-            fs.writeFileSync(path,JSON.stringify(misProductos, null, '\t'))
-        }else{
-            const data = fs.readFileSync(path, 'utf-8')
-            misProductos = JSON.parse(data)
-            console.log(misProductos)
-            const iD = misProductos.length + 1
-            const input = {
-                titulo: obj.titulo,
-                precio: obj.precio,
-                id: iD
-            }
-            misProductos.push(input)
-            fs.writeFileSync(path,JSON.stringify(misProductos, null, '\t'))
         }
+        getById = (id) =>{
+            const data = fs.readFileSync(path, 'utf-8');
+            const producto = JSON.parse(data)
+            console.log(producto[id-1])
+            
+        }
+        deleteById = (id) =>{
+            const data = fs.readFileSync(path, 'utf-8');
+            const productos = JSON.parse(data)
+            const deleteado = productos.filter((e) => e.id != id );
+            fs.writeFileSync(path, JSON.stringify(deleteado, null, '\t'))
 
-
-
-    }
+        }
 }
 
 const producto1 = new Contenedor()
 
-producto1.save({titulo: 'porfaaa', precio: 15})
+// producto1.save({ titulo: 'goma', precio: 70 })
+producto1.getById(1)
+producto1.deleteById(2)
 
